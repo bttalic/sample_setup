c65e2df3ae227c31858a4bb774bbf94a4c031e5b
Benjamin Talic <b.ttalic@gmail.com>
2016-02-03 08:09:09 +0100

    sample data

--- karma.conf.js
+++ karma.conf.js
@@ -22,24 +22,10 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [    
         //<!-- testfiles-dep -->   
-            "./server/vendor/angular.js",
-            "./server/vendor/ng-infinite-scroll.js",
-            "./server/vendor/jquery.js",
-            "./server/vendor/jcs-auto-validate.js",
-            "./server/vendor/bootstrap.min.js",
-            "./server/vendor/angular-ui-router.js",
-            "./server/vendor/angular-toastr.tpls.js",
-            "./server/vendor/angular-storage.js",
-            "./server/vendor/angular-sanitize.js",
-            "./server/vendor/angular-resource.js",
-            "./server/vendor/angular-mocks.js",
-            "./server/vendor/angular-cookies.js",
-            "./server/vendor/angular-animate.min.js",               
+                   
         //<!-- endinject-dep -->
         //<!-- testfiles -->       
-            "./T1/spec/sample.spec.js",
-            "./T1/src/index.module.js",
-            "./T1/src/index.controller.js"                          
+                                   
             //<!-- endinject -->
         ],
         // list of files / patterns to exclude
